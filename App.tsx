2fc028ba0ed84b9aab1e1ce39c9563d89acf6f7f
juliane <s215843@student.dtu.dk>
2024-09-24 16:09:24 +0200

    knap og Mui knap (hent Mui libary)

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>Welcome to my React Native App!</Text>
+    </View>
+  );
+};
+
+export default App;
