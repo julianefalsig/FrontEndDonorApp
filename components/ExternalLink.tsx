9718a0165a1fa0100b7317abafa86695165dcf43
evakathov <evakathov@hotmail.com>
2024-10-15 14:26:08 +0200

    Import React tilføjet til de fleste filer

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -1,24 +1,29 @@
-import { Link } from 'expo-router';
-import { openBrowserAsync } from 'expo-web-browser';
-import { type ComponentProps } from 'react';
-import { Platform } from 'react-native';
+import React from "react";
+import { Link, type Href } from "expo-router"; // Import the correct Href type
+import { openBrowserAsync } from "expo-web-browser";
+import { type ComponentProps } from "react";
+import { Platform } from "react-native";
 
-type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
+type Props = Omit<ComponentProps<typeof Link>, "href"> & {
+  href: Href<string | object>; // Use the correct Href type
+};
 
 export function ExternalLink({ href, ...rest }: Props) {
+  const handlePress = async (event: any) => {
+    if (Platform.OS !== "web") {
+      // Prevent the default behavior of linking to the default browser on native.
+      event.preventDefault();
+      // Open the link in an in-app browser.
+      await openBrowserAsync(href.toString());
+    }
+  };
+
   return (
     <Link
       target="_blank"
       {...rest}
       href={href}
-      onPress={async (event) => {
-        if (Platform.OS !== 'web') {
-          // Prevent the default behavior of linking to the default browser on native.
-          event.preventDefault();
-          // Open the link in an in-app browser.
-          await openBrowserAsync(href);
-        }
-      }}
+      onPress={handlePress} // Use handlePress function for event
     />
   );
 }
